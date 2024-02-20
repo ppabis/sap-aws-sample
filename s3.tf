09021bcab56ab3f0f6666b7ffe0b93261387662e
ppabis <piotr@pabis.eu>
2024-02-20 18:00:03 +0100

    Created unique S3 buckets for CF templates and for SAP

---
+++ s3.tf
@@ -0,0 +1,15 @@
+resource "random_string" "bucket-id" {
+  length  = 6
+  special = false
+  upper   = false
+  lower   = true
+  numeric = true
+}
+
+resource "aws_s3_bucket" "cf-templates" {
+  bucket = "sap-cf-templates-${random_string.bucket-id.result}"
+}
+
+resource "aws_s3_bucket" "sap-packages" {
+  bucket = "launchwizard-sap-packages-${random_string.bucket-id.result}"
+}
