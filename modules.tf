5fa9d1b210281f3b1cd304cb91ae7e3745dea928
ppabis <piotr@pabis.eu>
2024-02-23 21:20:32 +0100

    Add VPC module

--- modules.tf
+++ modules.tf
@@ -4,3 +4,11 @@ module "vpc" {
   VPC-cidr = "10.65.0.0/16"
   VPC-name = "SAP-VPC"
 }
+
+module "NAT" {
+  source         = "git::https://github.com/ppabis/nat-instance.git//NAT?ref=v2.0.0"
+  vpc            = module.vpc.VPC-id
+  public_subnet  = module.vpc.public-subnet-ids[0]
+  private_subnet = module.vpc.private-subnet-ids
+  route_tables   = [module.vpc.private-rtb-id]
+}
